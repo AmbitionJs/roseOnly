be8b47aefce80772c56b62a98819d601c6de84ae
Ambitions <544801019@qq.com>
2019-04-18 14:41:33 +0800

    main.js更新

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 import axios from 'axios'
 Vue.prototype.$http = axios
+
+Vue.use(ElementUI, { size: 'small', zIndex: 3000 });
 
 Vue.config.productionTip = false
 
@@ -12,4 +16,4 @@ new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app') 
