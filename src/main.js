c79e01ebc8ae9234f0ed4a07548f50573f9e6b6f
Jayce <13689649260@163.com>
2019-04-18 18:59:57 +0800

    1. 在 main.js 中添加 axios 的路由拦截 2. 在 main.js 中添加 axios 的相应拦截 3. 在 main.js 中添加 axios 的统一请求前缀

--- src/main.js
+++ src/main.js
@@ -11,6 +11,44 @@ import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(ElementUI);
 
+// 统一设置请求的前缀
+axios.defaults.baseURL = 'http://192.168.2.220:8081'
+
+// 请求拦截
+axios.interceptors.request.use(config => {
+  // 每次发请求之前都需要判断token
+  // 如果存在，则在请求头上带上token，一起发给后台验证
+  const token = localStorage.getItem('token')
+  token && (config.headers["Authorization"] = "Bearer " + token)
+  return config
+}, error => {
+  return Promise.reject(error);
+})
+
+// 响应拦截
+axios.interceptors.response.use(response => {
+  if (response.data.token) {
+    // 更新token
+    localStorage.setItem('token', response.data.token)
+  }
+  return response;
+}, error => {
+  let res = error.response
+  switch (res.status) {
+    case 401:
+      // token 验证失败,需要重新登陆
+      localStorage.removeItem('token')
+      return router.replace({
+        path: '/login'
+      })
+    case 404:
+      console.log('404错误')
+      break
+  }
+
+  return Promise.reject(error);
+});
+
 Vue.config.productionTip = false
 
 new Vue({
