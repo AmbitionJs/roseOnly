143f9d3851b3776abc6e917f671b761ba6acb60f
519664659 <1025724828@qq.com>
2019-04-19 15:31:17 +0800

    添加提交订单页面

---
+++ src/views/zjl/SubmitOrder.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="submit-order">
+    <div class="submit-order-title">
+      <h3>填写核对订单信息</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
