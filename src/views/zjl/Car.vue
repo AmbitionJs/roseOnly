4b7fde04e64d13ebb6daee849bff810352d8eb2d
519664659 <1025724828@qq.com>
2019-04-19 15:12:14 +0800

    完成购物车界面及功能

--- src/views/zjl/Car.vue
+++ src/views/zjl/Car.vue
@@ -5,7 +5,7 @@
         <span>我的购物车</span>
       </div>
       <div class="cart-title-right">
-        <a href class="cart-clear-btn" @click="deleteCar()">
+        <a href="javascript:;" class="cart-clear-btn" @click="clearCart()">
           <i class="el-icon-delete"></i>
           <span>清空购物车</span>
         </a>
@@ -21,148 +21,270 @@
           <th>数量</th>
           <th>操作</th>
         </tr>
-        <tr v-for="item in cardatas" :key="item.carId">
+        <tr v-for="item in cardatas" :key="item.goodsIds">
           <td>
-            <input type="checkbox">
+            <input type="checkbox" :checked="item.select" @click="item.select=!item.select">
           </td>
-          <td>{{item.brand}}</td>
+          <td>roseonly</td>
           <td>
-            <img src alt>
-            <span>{{item.goodname}}</span>
+            <img :src="item.picFileUrl" alt>
+            <span>{{item.goodsName}}</span>
           </td>
-          <td>￥{{item.price}}</td>
+          <td>￥{{item.goodsPrice}}</td>
           <td>
             <div class="number-group">
-              <span class="el-icon-remove-outline" ></span>
-              <input type="text" v-model="item.number" class="good-number">
+              <span class="el-icon-remove-outline" @click="reduceNum(item.goodsIds)"></span>
+              <input
+                type="text"
+                v-model="item.goodsNum"
+                class="good-number"
+                @change="changeNum(item.goodsIds)"
+              >
               <span class="el-icon-circle-plus-outline" @click="addNum(item.goodsIds)"></span>
             </div>
           </td>
           <td>
-            <a href="" class="del-shopping-list" @click="deleteCar(item.goodsIds)">删除</a>
+            <span class="del-shopping-list" @click="deleteList(item.goodsIds)">删除</span>
           </td>
         </tr>
       </table>
     </div>
     <div class="cart-footer">
       <div class="cart-footer-left">
-        <input type="checkbox" id="check-all">
-        <label for="check-all">全选</label>
+        <input
+          type="checkbox"
+          id="select-all"
+          @change="selectAll(isSelectAll)"
+          :checked="isSelectAll"
+        >
+        <label for="select-all">全选</label>
       </div>
       <div class="cart-footer-right">
         <div class="total-price">
           合计：￥
-          <span>{{totalPrice}}</span>
+          <span>{{getTotal}}</span>
         </div>
-        <button type="button" id="settlement">去结算</button>
+        <button type="button" id="settlement" @click="settlement">去结算</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// import ShoppingList from "@/components/zjl/ShoppingList.vue"
-// import { constants } from 'crypto'
-
 export default {
   name: "cart",
   data() {
     return {
-      totalPrice: "",
       cardatas: [
         {
-          carId: 1,
+          goodsIds: 1,
           brand: "roseonly",
-          goodname: "玫瑰公仔-项圈狗手链与甜心狗",
-          url: "",
-          price: 1999,
-          number: 1
+          goodsName: "玫瑰公仔-项圈狗手链与甜心狗",
+          picFileUrl: "",
+          goodsPrice: 1999,
+          goodsNum: 1
         },
         {
-          carId: 2,
+          goodsIds: 2,
           brand: "roseonly",
-          goodname: "玫瑰公仔-项圈狗手链与甜心狗",
-          url: "",
-          price: 1999,
-          number: 1
+          goodsName: "玫瑰公仔-项圈狗手链与甜心狗",
+          picFileUrl: "",
+          goodsPrice: 1999,
+          goodsNum: 1
+        },
+        {
+          goodsIds: 3,
+          brand: "roseonly",
+          goodsName: "玫瑰公仔-项圈狗手链与甜心狗",
+          picFileUrl: "",
+          goodsPrice: 1999,
+          goodsNum: 1
+        },
+        {
+          goodsIds: 4,
+          brand: "roseonly",
+          goodsName: "玫瑰公仔-项圈狗手链与甜心狗",
+          picFileUrl: "",
+          goodsPrice: 2999,
+          goodsNum: 2
         }
       ]
-    }
+    };
   },
   created() {
     // 获取购物车数据
-    const token = sessionStorage.getItem("token")
-    const userId = sessionStorage.getItem("userId")
-    if(token) {
-      this.axios.get("/trolley/" + userId, {
-        userId: userId,
-        userToken: token
-      })
-      .then(res => {
-        console.log(res)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-    }
-  },
-  methods: {
-    // 清空购物车或删除单个
-    deleteCar(id) {
-      var goodsIds = []
-      if (id) {
-        goodsIds.push(id)
-      }
-      console.log(id, goodsIds)
-      const token = sessionStorage.getItem("token")
-      if(token) {
-        this.axios.post("/trolley/delete", {
-          goodsIds: goodsIds,
+    const token = sessionStorage.getItem("token");
+    const userId = sessionStorage.getItem("userId");
+    if (token) {
+      this.axios
+        .get("/trolley/" + userId, {
+          userId: userId,
           userToken: token
         })
         .then(res => {
-          console.log(res)
+          console.log(res);
         })
         .catch(err => {
-          console.log(err)
-        })
+          console.log(err);
+        });
+    }
+  },
+  methods: {
+    // 删除单个购物车商品
+    deleteList(id) {
+      var goodsIds = [];
+      if (id) {
+        goodsIds.push(id);
+      }
+      console.log(goodsIds);
+      const token = sessionStorage.getItem("token");
+      if (token) {
+        this.axios
+          .post("/trolley/delete", {
+            goodsIds: goodsIds,
+            userToken: token
+          })
+          .then(res => {
+            console.log(res);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }
+      this.cardatas.forEach((item,index) => {
+        if(item.goodsIds == id) {
+          this.cardatas.splice(index, 1)
+        }
+      })
+    },
+    // 清空购物车
+    clearCart() {
+      this.cardatas = []
+      const token = sessionStorage.getItem("token");
+      if (token) {
+        this.axios
+          .post("/trolley/delete", {
+            userToken: token
+          })
+          .then(res => {
+            console.log(res);
+          })
+          .catch(err => {
+            console.log(err);
+          });
       }
     },
+    // 增加商品数量
     addNum(id) {
-      var num = ''
-      const token = sessionStorage.getItem("token")
-      this.axios.post('/trolley/add', {
-        goodsId: id,
-        goodsNum: num,
-        userToken: token
-      })
+      var num;
+      this.cardatas.forEach(item => {
+        if (item.goodsIds == id) {
+          item.goodsNum++;
+          num = item.goodsNum;
+        }
+      });
+      console.log(num);
+      // const token = sessionStorage.getItem("token")
+      // this.axios.post('/trolley/add', {
+      //   goodsId: id,
+      //   goodsNum: num,
+      //   userToken: token
+      // })
     },
+    // 减少商品数量
     reduceNum(id) {
-      var num = ''
-      const token = sessionStorage.getItem("token")
-      this.axios.post('/trolley/add', {
-        goodsId: id,
-        goodsNum: num,
+      var num;
+      this.cardatas.forEach(item => {
+        if (item.goodsIds == id) {
+          if (item.goodsNum == 1) {
+            num = item.goodsNum;
+            return;
+          }
+          item.goodsNum--;
+          num = item.goodsNum;
+        }
+      });
+      console.log(num);
+      // const token = sessionStorage.getItem("token")
+      // this.axios.post('/trolley/add', {
+      //   goodsId: id,
+      //   goodsNum: num,
+      //   userToken: token
+      // })
+    },
+    // 输入商品数量
+    changeNum(id) {
+      var num;
+      this.cardatas.forEach(item => {
+        if (item.goodsIds == id) {
+          if (item.goodsNum <= 0) {
+            item.goodsNum = 1;
+          }
+          num = item.goodsNum;
+        }
+      });
+      console.log(num);
+      // const token = sessionStorage.getItem("token")
+      // this.axios.post('/trolley/add', {
+      //   goodsId: id,
+      //   goodsNum: num,
+      //   userToken: token
+      // })
+    },
+    // 全选/取消全选
+    selectAll(_isSelect) {
+      this.cardatas.forEach(item => {
+        item.select = !_isSelect;
+      });
+    },
+    // 结算
+    settlement() {
+      var ids = [],token = sessionStorage.getItem('token')
+      this.cardatas.forEach(item => {
+        if (item.select) {
+          ids.push(item.goodsIds);
+        }
+      });
+      console.log(ids);
+      this.axios.post('/trolley/settlement', {
+        trolleyIds: ids,
         userToken: token
       })
-    },
-    checkAll() {
-      
+      .then(res => {
+        if(res.state == 200) {
+          this.$router.push('/submitOrder')
+        }
+      })
     }
   },
   computed: {
-    // getTotal() {
-
-    // }
+    // 检测是否全选
+    isSelectAll() {
+      return this.cardatas.every(item => {
+        return item.select;
+      });
+    },
+    getTotal() {
+      // 获取cardatas中select为true的数据
+      var items = this.cardatas.filter(item => {
+          if (item.select) return item;
+        }),
+        totalPrice = 0;
+      // 计算总价
+      items.forEach(item => {
+        totalPrice += item.goodsPrice * item.goodsNum;
+      });
+      return totalPrice;
+    }
   },
-  mounted: function () {
-    var _this = this
+  mounted() {
+    var _this = this;
     //为cardatas添加select（是否选中）字段，初始值为true
-    this.cardatas.map(function (item) {
-      _this.$set(item, 'select', true)
-    })
+    this.cardatas.map(function(item) {
+      _this.$set(item, "select", true);
+    });
   }
-}
+};
 </script>
 
 <style>
@@ -170,6 +292,7 @@ export default {
   width: 968px;
   min-width: 768px;
   margin: 0 auto;
+  user-select: none;
 }
 .cart-title-left {
   color: #414141;
@@ -233,6 +356,9 @@ tr {
   display: flex;
   justify-content: space-between;
 }
+tr:not(:first-child) {
+  line-height: 80px;
+}
 .good-number {
   width: 100px;
   text-align: center;
@@ -241,8 +367,18 @@ th,
 td {
   padding: 10px;
 }
+.number-group {
+  user-select: none;
+}
 .number-group span {
   padding: 10px;
+  cursor: pointer;
+}
+.del-shopping-list {
+  padding: 10px 0;
+}
+.del-shopping-list:hover {
+  text-decoration: underline;
   cursor: pointer;
 }
 </style>
