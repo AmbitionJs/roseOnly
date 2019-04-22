3b1e933b9bb210c40f8b5d2091905c49c96ca2e7
Jayce <13689649260@163.com>
2019-04-22 11:29:45 +0800

    修改个人信息页面为两部分

--- src/components/hsj/Info.vue
+++ src/components/hsj/Info.vue
@@ -5,71 +5,79 @@
       <el-tab-pane>
         <span slot="label">基本信息</span>
         <el-form ref="form" :model="form" label-width="80px">
-          <!-- 我的信息 -->
-          <el-form-item label="我的信息"></el-form-item>
-          <el-form-item label="姓名">
-            <el-input v-model="form.name"></el-input>
-          </el-form-item>
-          <el-form-item label="手机">
-            <el-input v-model="form.phone"></el-input>
-          </el-form-item>
-          <el-form-item label="生日">
-            <el-col>
-              <el-date-picker
-                type="date"
-                placeholder="选择日期"
-                v-model="form.birth"
-                style="width: 100%;"
-              ></el-date-picker>
-            </el-col>
-          </el-form-item>
-          <el-form-item label="性别">
-            <el-radio-group v-model="form.gender">
-              <el-radio label="男"></el-radio>
-              <el-radio label="女"></el-radio>
-            </el-radio-group>
-          </el-form-item>
-          <el-form-item label="居住地">
-            <el-select v-model="form.region" placeholder="请选择居住地">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
-            </el-select>
-            <el-select v-model="form.region" placeholder="请选择居住地">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
-            </el-select>
-            <el-select v-model="form.region" placeholder="请选择居住地">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
-            </el-select>
-          </el-form-item>
-          <!-- 爱人信息 -->
-          <el-form-item label="爱人信息"></el-form-item>
-          <el-form-item label="姓名">
-            <el-input v-model="lover.name"></el-input>
-          </el-form-item>
-          <el-form-item label="手机">
-            <el-input v-model="lover.phone"></el-input>
-          </el-form-item>
-          <el-form-item label="生日">
-            <el-col>
-              <el-date-picker
-                type="date"
-                placeholder="选择日期"
-                v-model="lover.birth"
-                style="width: 100%;"
-              ></el-date-picker>
-            </el-col>
-          </el-form-item>
-          <el-form-item label="性别">
-            <el-radio-group v-model="lover.gender">
-              <el-radio label="男"></el-radio>
-              <el-radio label="女"></el-radio>
-            </el-radio-group>
-          </el-form-item>
-          <el-form-item>
-            <el-button>提交</el-button>
-          </el-form-item>
+          <div style="overflow: hidden;">
+            <!-- 我的信息 -->
+            <div class="detail-info user-info">
+              <el-form-item label="我的信息"></el-form-item>
+              <el-form-item label="姓名">
+                <el-input v-model="form.name"></el-input>
+              </el-form-item>
+              <el-form-item label="手机">
+                <el-input v-model="form.phone"></el-input>
+              </el-form-item>
+              <el-form-item label="生日">
+                <el-col>
+                  <el-date-picker
+                    type="date"
+                    placeholder="选择日期"
+                    v-model="form.birth"
+                    style="width: 100%;"
+                  ></el-date-picker>
+                </el-col>
+              </el-form-item>
+              <el-form-item label="性别">
+                <el-radio-group v-model="form.gender">
+                  <el-radio label="男"></el-radio>
+                  <el-radio label="女"></el-radio>
+                </el-radio-group>
+              </el-form-item>
+              <el-form-item label="居住地">
+                <el-select v-model="form.region" placeholder="请选择居住地">
+                  <el-option label="区域一" value="shanghai"></el-option>
+                  <el-option label="区域二" value="beijing"></el-option>
+                </el-select>
+                <el-select v-model="form.region" placeholder="请选择居住地">
+                  <el-option label="区域一" value="shanghai"></el-option>
+                  <el-option label="区域二" value="beijing"></el-option>
+                </el-select>
+                <el-select v-model="form.region" placeholder="请选择居住地">
+                  <el-option label="区域一" value="shanghai"></el-option>
+                  <el-option label="区域二" value="beijing"></el-option>
+                </el-select>
+              </el-form-item>
+            </div>
+            <!-- 爱人信息 -->
+            <div class="detail-info lover-info">
+              <el-form-item label="爱人信息"></el-form-item>
+              <el-form-item label="姓名">
+                <el-input v-model="lover.name"></el-input>
+              </el-form-item>
+              <el-form-item label="手机">
+                <el-input v-model="lover.phone"></el-input>
+              </el-form-item>
+              <el-form-item label="生日">
+                <el-col>
+                  <el-date-picker
+                    type="date"
+                    placeholder="选择日期"
+                    v-model="lover.birth"
+                    style="width: 100%;"
+                  ></el-date-picker>
+                </el-col>
+              </el-form-item>
+              <el-form-item label="性别">
+                <el-radio-group v-model="lover.gender">
+                  <el-radio label="男"></el-radio>
+                  <el-radio label="女"></el-radio>
+                </el-radio-group>
+              </el-form-item>
+            </div>
+          </div>
+          <div class="submit">
+            <el-form-item>
+              <el-button>提交</el-button>
+            </el-form-item>
+          </div>
         </el-form>
       </el-tab-pane>
       <!-- 修改密码 -->
@@ -105,6 +113,13 @@
 }
 .box-card:nth-child(2) {
   float: right;
+}
+.detail-info {
+  width: 50%;
+  float: left;
+}
+.submit {
+  text-align: center;
 }
 </style>
 
