33885d509dfa297e5cdc9eaf47ee119a8807551b
Ambitions <544801019@qq.com>
2019-04-25 14:58:35 +0800

    准备联通后台开始测试数据

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,63 @@
 <template>
   <div id="app">
-    <router-view/>
+    
+    <Nav />
+
+    <div :class="[{mtop: topdis},'container']">
+      <router-view/>
+    </div>
+    
+
+    <Footer />
+    
   </div>
 </template>
 
-<style>
+<script>
+import Nav from '@/components/hjs/Navigation.vue'
+import Footer from '@/components/hjs/Footer.vue'
 
+export default {
+  data(){
+    return {
+      topdis:false
+    }
+  },
+  components:{
+    Nav,
+    Footer,
+  },
+  created(){
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  methods:{
+    // 滚轮滑动距离
+    handleScroll() {
+      var scrollTop =
+        window.pageYOffset ||
+        document.documentElement.scrollTop ||
+        document.body.scrollTop;
+      scrollTop < 100 ? (this.topdis = false) : (this.topdis = true);      
+    },
+  }
+}
+</script>
+
+
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  ul,li{
+    list-style: none;
+  }
+  .container{
+    width: 1170px;
+    margin: 0 auto;
+  }
+  .mtop{
+    margin-top: 150px;
+  }
 </style>
+
