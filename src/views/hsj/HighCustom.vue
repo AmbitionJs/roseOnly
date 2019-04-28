2afee14381892d53cd06410b080255c23394752f
贺斯洁 <13689649260@163.com>
2019-04-28 10:13:44 +0800

    添加高端定制

---
+++ src/views/hsj/HighCustom.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hign-custom">
+    <!-- 高端定制对应图片 -->
+    <div class="top">
+      <img src="@/assets/hsj/1.png" alt>
+    </div>
+    <!-- 高端定制商品 -->
+    <el-row>
+      <el-col :span="24">
+        <div class="list">
+          <img src="@/assets/hsj/2.jpg" alt>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <div class="list">
+        <img src="@/assets/hsj/3.jpg" alt>
+      </div>
+      <div class="list">
+        <img src="@/assets/hsj/4.jpg" alt>
+      </div>
+    </el-row>
+  </div>
+</template>
+
+<style scoped>
+.list {
+  float: left;
+}
+</style>
+
+
+
